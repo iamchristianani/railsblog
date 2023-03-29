6b36556096bef27ba76f9f983f81c95188aba50f
Christian Ani <iamchristianani@gmail.com>
2023-03-29 18:56:21 +0100

    Create custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :update_post_comments_counter
+
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
