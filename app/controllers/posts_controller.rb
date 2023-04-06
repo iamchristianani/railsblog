6687219c22a2d7b8dffec41f5bbba64479f93a34
Christian Ani <iamchristianani@gmail.com>
2023-04-06 11:48:05 +0100

    Update Posts controller to retrieve all Posts and find User

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.all
+    @user = User.find(params[:user_id])
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+  end
 end
