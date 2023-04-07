1bed8b393498405f6d0c830c4492e5f5fd7de861
Christian Ani <iamchristianani@gmail.com>
2023-04-07 13:07:08 +0100

    Create comments controller to add new a comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment.post_id = @post.id
+    if @comment.save
+      flash[:success] = 'Comment created successfully'
+    else
+      flash[:danger] = "Couldn't create comment"
+    end
+    redirect_to "/users/#{current_user.id}/posts/#{params[:post_id]}"
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
