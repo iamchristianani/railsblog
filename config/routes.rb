2dff89dddfceb722f8d387085dfca3f6ef6cbfd0
Christian Ani <iamchristianani@gmail.com>
2023-04-07 15:51:12 +0100

    Update route to make a lean code, and follow latest Rails syntax

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "users#index"
   
-  get 'posts/new', to: 'posts#new'
-  post 'posts', to: 'posts#create'
-  post 'comments/:post_id', to: 'comments#create'
-  post 'likes/:post_id', to: 'likes#create'
-  
   resources :users, only: [:index, :show]  do
-    resources :posts, only: [:index, :show] 
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 end
