d940909cb9c0ee9852727fa2359b669d5f5d004a
Christian Ani <iamchristianani@gmail.com>
2023-04-07 13:08:04 +0100

    Update routing for new posts, comments, and likes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "users#index"
   
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+  post 'comments/:post_id', to: 'comments#create'
+  post 'likes/:post_id', to: 'likes#create'
+  
   resources :users, only: [:index, :show]  do
     resources :posts, only: [:index, :show] 
   end
