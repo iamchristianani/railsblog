6cff06eaa13e90bbffa3f12e6efcf694e8558271
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-10 18:00:33 +0200

    Add rspec test using capybara for posts view index template

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,24 +1,21 @@
 require 'rails_helper'
-
-RSpec.describe "posts/index", type: :view do
+RSpec.describe "posts/index", type: :feature do
   before :each do
-    @img = 'https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2022/10/Andor_Luthen-Rael.png?fit=2554,1642&quality=60&strip=all&ssl=1',
-    @author = User.create(name: 'Tom', photo: @img, bio: 'Teacher from South Africa'),
+    @img = 'https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2022/10/Andor_Luthen-Rael.png?fit=2554,1642&quality=60&strip=all&ssl=1'
+    @author = User.create(name: 'Tom', photo: @img, bio: 'Teacher from South Africa', posts_counter: 2)
     @first_post = Post.create(author: @author, title: 'Tom Post #1', text: "This is Tom's first post")
     @second_post = Post.create(author: @author, title: 'Tom Post #2', text: "This is Tom's second post")
-
     @first_comment = Comment.create(post: @first_post, author: @author, text: 'First comment for Tom' )
     @second_comment = Comment.create(post: @second_post, author: @author, text: 'Second comment for Tom' )
-
     @first_like = Like.create(post: @first_post, author: @author)
     @second_like = Like.create(post: @second_post, author: @author)
-
-    # visit user_posts_path(@author)
+    visit user_posts_path(@author)
     # click_on @author.name 
   end
 
   it 'shows the author image' do
-    expect(page.body).to include(@img)
+    img = find(".user-image")
+    expect(img[:src]).to include(@img)
   end
 
   it 'shows the author name' do
@@ -26,7 +23,7 @@ RSpec.describe "posts/index", type: :view do
   end
 
   it 'shows the number of posts by the author' do
-    expect(page).to have_content('2 posts')
+    expect(page).to have_content('Number of posts: 2')
   end
 
   it 'shows the post title' do
@@ -42,13 +39,13 @@ RSpec.describe "posts/index", type: :view do
   end
 
   it 'shows the number of comments on the post' do
-    expect(page).to have_content('1 comment')
+    expect(page).to have_content('Comments: 1')
   end
 
   it 'shows the number of likes on the post' do
-    expect(page).to have_content('1 like')
+    expect(page).to have_content('Likes: 1')
   end
-
+  
   it "when clicking on a post title, it redirects to the post's show page" do
     click_on @first_post.title
     expect(page).to have_content(@first_post.title)
