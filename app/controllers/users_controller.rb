1268cb4d7d2ae08671f6b1c67bb3bb811f6bf5f7
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-12 16:44:58 +0200

    Fix rubocop errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @users = User.all.order(created_at: :asc)
     @current_user = current_user
