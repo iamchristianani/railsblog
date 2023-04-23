f32846ee46e4b1e8586951c61322385fc3bf3049
Christian Ani <iamchristianani@gmail.com>
2023-04-23 22:58:05 +0100

    Add destroy as dependent to linked database columns

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true
   validates :title, length: { maximum: 250 }
@@ -9,6 +9,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter
+  after_destroy :update_posts_counter
 
   def initialize(attributes = {})
     super
